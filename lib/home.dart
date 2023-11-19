66f50a41ea3ceb166d59483a6d25123588657081
Lakshay-Ahlawat <lakshay6447@gmail.com>
2023-11-20 01:03:06 +0530

    :bug: production bug fixing

--- lib/home.dart
+++ lib/home.dart
@@ -1431,8 +1431,9 @@ class _HomeScreenState extends State<HomeScreen> {
                                                     padding:
                                                         EdgeInsets.all(4.0),
                                                     child: Text(
-                                                      bubbleText[index]
-                                                          .toString(),
+                                                      // bubbleText[index]
+                                                      //     .toString(),
+                                                      "yes",
                                                       style:
                                                           GoogleFonts.bangers(),
                                                     ),
